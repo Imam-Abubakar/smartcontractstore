18827d9e9f16d5e40f2da1e2de57c5a27b740a8a
Imam Abubakar <54324954+Imam-Abubakar@users.noreply.github.com>
2022-06-09 18:46:35 +0100

    No 4

--- learn-solidity-in-30-programs/SimpleEvaluation.sol
+++ learn-solidity-in-30-programs/SimpleEvaluation.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 contract day4 {
     function evaluate(int256 x, int256 y) public pure returns (int256){
