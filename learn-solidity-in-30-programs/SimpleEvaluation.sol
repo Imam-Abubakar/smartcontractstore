cdeb610917a16508c2f55cfbbcdef3545fcc1510
Imam Abubakar <54324954+Imam-Abubakar@users.noreply.github.com>
2022-06-09 18:45:52 +0100

    Update SimpleEvaluation.sol

--- learn-solidity-in-30-programs/SimpleEvaluation.sol
+++ learn-solidity-in-30-programs/SimpleEvaluation.sol
@@ -2,8 +2,8 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract day4 {
-    function evaluate(int256 a, int256 b) public pure returns (int256){
-    return ((a+b) - (a-b));
+    function evaluate(int256 x, int256 y) public pure returns (int256){
+    return ((x+y) - (x-y));
 }
 
 }
